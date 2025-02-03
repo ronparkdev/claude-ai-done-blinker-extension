4215ca90ce91e6c7d84482f865cda340e06ab4f3
Ron Park <ronpark.dev@gmail.com>
2025-02-03 15:13:56 +0900

    fix: not working beween home and chat page

--- content.js
+++ content.js
@@ -1,27 +1,48 @@
 (() => {
   let isPrevRunning = null;
 
+  function send(isRunning) {
+    chrome.runtime.sendMessage({
+      type: "STATUS_CHANGE",
+      isRunning: isRunning,
+      action: "PLAY_SOUND",
+    });
+  }
+
   // 상태 체크 및 처리
   function checkStatus() {
-    const hasElement = document.querySelector("svg.w-full.fill-current");
-    if (isPrevRunning === null && !hasElement) {
+    const isChatPage = new URL(window.location.href).pathname.startsWith(
+      "/chat/"
+    );
+
+    const svgElement = document.querySelector(
+      "div.w-8.text-accent-brand.inline-block.select-none svg"
+    );
+
+    if (!svgElement) {
       return;
     }
 
-    const isRunning = !hasElement;
-
-    if (isPrevRunning !== isRunning) {
-      if (isPrevRunning !== null) {
-        // background script에 메시지 전송 (오디오 재생 포함)
-        chrome.runtime.sendMessage({
-          type: "STATUS_CHANGE",
-          isRunning: isRunning,
-          action: "PLAY_SOUND",
-        });
-      }
-      console.log(isRunning ? "Running" : "Not Running");
-      isPrevRunning = isRunning;
+    const viewBoxOffset = svgElement
+      .getAttribute("viewBox")
+      .split(" ")
+      .map(Number)[3];
+    if (Number.isNaN(viewBoxOffset)) {
+      return;
     }
+
+    const isRunning = isChatPage && viewBoxOffset !== 100;
+
+    if (isPrevRunning === isRunning) {
+      return;
+    }
+
+    if (isPrevRunning !== null && isChatPage) {
+      send(isRunning);
+    }
+
+    // console.log(isRunning ? "Running" : "Not Running");
+    isPrevRunning = isRunning;
   }
 
   // 주기적으로 상태 체크
